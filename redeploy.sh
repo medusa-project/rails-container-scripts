758fabc8f68da3685a35a73c200e849de18fcab6
Alex Dolski <alexd@n-ary.net>
2019-11-25 13:38:05 -0600

    Exit early if any of the commands fail

--- redeploy.sh
+++ redeploy.sh
@@ -7,5 +7,15 @@ then
 fi
 
 rails-container-scripts/docker-build.sh $1
+if [ $? -ne 0 ]
+    echo "Failed to build the image"
+    exit 1
+fi
+
 rails-container-scripts/ecr-push.sh $1
+if [ $? -ne 0 ]
+    echo "Failed to push the image to ECR"
+    exit 1
+fi
+
 rails-container-scripts/ecs-deploy-webapp.sh $1
