daeb2ec4fa2b588eae4c4dec6f044d1ea088fafd
Alex Dolski <alexd@n-ary.net>
2019-11-27 14:55:28 -0600

    Fix syntax errors

--- redeploy.sh
+++ redeploy.sh
@@ -8,12 +8,14 @@ fi
 
 rails-container-scripts/docker-build.sh $1
 if [ $? -ne 0 ]
+then
     echo "Failed to build the image"
     exit 1
 fi
 
 rails-container-scripts/ecr-push.sh $1
 if [ $? -ne 0 ]
+then
     echo "Failed to push the image to ECR"
     exit 1
 fi
