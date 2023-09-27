d756c1bd41b000ed2a342cf03f0b1a42fd9be22f
Alex Dolski <alexd@n-ary.net>
2023-09-27 17:05:04 -0500

    Build ARM & x86 images using docker buildx

--- docker-build.sh
+++ docker-build.sh
@@ -9,5 +9,7 @@ fi
 source rails-container-scripts/env.sh env-common.list
 source rails-container-scripts/env.sh env-$1.list
 
+eval $(aws ecr get-login --no-include-email --region $AWS_REGION --profile $AWS_PROFILE)
+
 docker buildx build --platform linux/amd64,linux/arm64 --push \
     -t $ECR_HOST/$IMAGE_NAME .
